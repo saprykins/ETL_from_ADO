ee849586a3fcc8c8bbe416859935f5ab3615670e
saprykins <60004824+saprykins@users.noreply.github.com>
2023-03-12 22:28:32 +0100

    Update _read_csv.py

--- _read_csv.py
+++ _read_csv.py
@@ -11,15 +11,18 @@ import openpyxl
 
 df_servers = pd.read_csv('./results/__tcs_servers_extract.csv')
 df_apps = pd.read_csv("./results/__tcs_applications_extract.csv")
+df_history = pd.read_csv("./results/__tcs_history.csv")
 # print(df_apps)
 
-# df3 = pd.merge(df_servers,df_apps, on=['App id in ADO'], how = "outer")
-df3 = pd.merge(df_servers,df_apps, on=['App id in ADO'])
-# print(df3)
+df3 = pd.merge(df_servers,df_apps, on=['App id in ADO'], how = "outer")
+df4 = pd.merge(df3, df_history, on=['App id in ADO'])
 
-df_tcs = df3.drop(["Unnamed: 0_x", "Server id in ADO", "Unnamed: 0_y", "App id in ADO"], axis=1)
+# print(df4.T)
+
+df_tcs = df4.drop(["Unnamed: 0_x", "Server id in ADO", "Unnamed: 0_y", "App id in ADO"], axis=1)
 # df4=df3.drop([df3.columns[0], df3.columns[1]], axis=1)
-
+# pritn(df_tcs)
+# df4.to_csv('./results/__xxx_tcs.csv',index=False)
 
 
 # for internal usage
@@ -27,7 +30,8 @@ df3_outer_join = pd.merge(df_servers,df_apps, on=['App id in ADO'], how = 'outer
 df4_outer_join = df3_outer_join.drop(["Unnamed: 0_x", "Unnamed: 0_y"], axis=1)
 # print(df4)
 
-# df4.to_csv('./results/__tcs_extract.csv',index=False)
+
+
 # print(df4)
 # df4.to_excel('test.xlsx', sheet_name='tcs')
 df4_outer_join.to_excel('./results/ADO_TCS_outer_join.xlsx', sheet_name='tcs_all', index=False)
@@ -41,6 +45,7 @@ df4_outer_join.to_excel('./results/ADO_TCS_outer_join.xlsx', sheet_name='tcs_all
 df_servers = pd.read_csv('./results/__ms_servers_extract.csv')
 df_apps = pd.read_csv("./results/__ms_applications_extract.csv")
 df_map = pd.read_csv("./results/__ms_mapping.csv")
+df_history = pd.read_csv("./results/__ms_history.csv")
 
 # print(df_servers)
 # print(df_apps)
@@ -50,9 +55,31 @@ df3 = pd.merge(df_servers,df_map, on=['Server id in ADO'])
 
 # df4 = pd.merge(df3,df_apps, on=['App id in ADO'], how = "outer")
 df4 = pd.merge(df3,df_apps, on=['App id in ADO'])
+df_4c = df4.drop(["Unnamed: 0", "Unnamed: 0_x", "Server id in ADO", "Unnamed: 0_y", "Unnamed: 0"], axis=1)
+# df_x.to_csv('./results/__xxx_ms.csv',index=False)
+
+
+
+
+df5 = pd.merge(df_4c, df_history, on=['App id in ADO'])
+# print(df5)
+# df5.to_csv('./results/__xxx_ms.csv',index=False)
+
+
+
 # print(df4)
 
-df_ms = df4.drop(["Unnamed: 0", "Unnamed: 0_x", "Server id in ADO", "Unnamed: 0_y", "Unnamed: 0", "App id in ADO"], axis=1)
+df_ms = df5.drop(["Unnamed: 0", "Unnamed: 0", "App id in ADO"], axis=1)
+# df_ms.to_csv('./results/__xxx_ms.csv',index=False)
+#
+#
+#
+# df_ms = df5.drop(["Server id in ADO", "Unnamed: 0_y", "Unnamed: 0", "App id in ADO"], axis=1)
+#
+#
+#
+
+
 # df_ms = df4.drop(["Unnamed: 0", "Unnamed: 0_x", "Unnamed: 0_y", "Unnamed: 0"], axis=1)
 # df4=df3.drop([df3.columns[0], df3.columns[1]], axis=1)
 # print(df5)
