8fd9c1f4f8bf8cb084602b9ddd7282c65232eba4
saprykins <60004824+saprykins@users.noreply.github.com>
2023-04-06 17:51:20 +0200

    Update post.py

--- post.py
+++ post.py
@@ -10,7 +10,7 @@ workitemtype = "task"
 
 # authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
 
-url = "https://dev.azure.com/" + organization + "/" + project + "/_apis/wit/workitems/$" + workitemtype + "?api-version=7.0"
+# url = "https://dev.azure.com/" + organization + "/" + project + "/_apis/wit/workitems/$" + workitemtype + "?api-version=7.0"
 url = "https://dev.azure.com/{}/{}/_apis/wit/workitems/${}?api-version=7.0".format(organization, project, workitemtype)
 
 headers = {
@@ -21,7 +21,30 @@ body = [
     {
     "op": "add",
     "path": "/fields/System.Title",
-    "value": "Sample Task 3"
+    "value": "Sample Task 8"
+    },
+    {
+    "op": "add",
+    "path": "/fields/System.Description",
+    "value": "has parent"
+    },
+    {
+    "op": "add",
+    "path": "/fields/System.IterationPath",
+    "value": "ADO Testing\\Sprint 1"
+    },
+    {
+    "op": "add",
+    "path": "/fields/System.Parent",
+    "value": "173988"
+    },
+    {
+    "op": "add",
+    "path": "/relations/-",
+    "value": {
+        "rel":"System.LinkTypes.Hierarchy-Reverse",
+        "url":"https://dev.azure.com/go-gl-pr-migfactory-axa365/1e294b14-8de0-43b1-962a-4fe4275e31c7/_apis/wit/workItems/173988"
+        }
     }
 ]
 
@@ -35,4 +58,3 @@ r = requests.post(
 
 
 print(r)
-
