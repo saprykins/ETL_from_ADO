30ea05caed0e75efc5b19a224016953750bab4e7
saprykins <60004824+saprykins@users.noreply.github.com>
2023-05-03 13:17:17 +0200

    Update ado_extractor.sh

--- ado_extractor.sh
+++ ado_extractor.sh
@@ -3,11 +3,15 @@ source ./venv/bin/activate
 
 ACCOUNT_KEY="1***"
 
+rm -f ./results/*.csv
+rm -f ./results/*.xlsx
 python _ms_etl_app_w_dates_end_date.py
 python _tcs_etl_app_w_dates_enddate.py
 python get_ms_history.py
 python get_tcs_history.py
 python _read_csv.py
+python replace_columns.py
+
 
 # sends files to Azure azuredevops-fs
 az storage file upload --account-name azuredevopsblobstorage --account-key $ACCOUNT_KEY --share-name azuredevops-fs --source results/ADO_TCS_extract.xlsx
